b75bc719dd0e00733e43a25be1ecf671391e95f5
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2020-06-25 13:07:49 +0100

    Implementing forecast page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutterweatherapp/styles.dart';
+import 'package:flutterweatherapp/utils/app_settings.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  AppSettings settings = AppSettings();
+
+  // Don't allow landscape mode
+  await SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((_) => runApp(MyApp(settings: settings)));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final AppSettings settings;
+
+  const MyApp({Key key, this.settings}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final theme = ThemeData(
+      fontFamily: "Cabin",
+      primaryColor: AppColor.midnightSky,
+      accentColor: AppColor.midnightCloud,
+      primaryTextTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+      textTheme: Theme.of(context).textTheme.apply(
+            bodyColor: AppColor.textColorDark,
+            displayColor: AppColor.textColorDark,
+          ),
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      debugShowCheckedModeBanner: false,
+      theme: theme,
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
